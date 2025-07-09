eef344d42f5008134dbc4304c60222c83d416f4a
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2025-07-10 00:28:11 +0900

    もろもろ

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 async function sendMidi() {
 	let startTime = 0;
 	const midiOutputs = await getMIDI();
-	const arrayBufferSource = await getAudioBufferSource(await getMp3ArrayBuffer("./BLACK SHOUT.mp3"));
-	let settings = parseLightData(await (await fetch("./settings.tsv")).text()).sort((a, b) => a.time - b.time);
+	const arrayBufferSource = await getAudioBufferSource(await getMp3ArrayBuffer("./data/BLACK SHOUT.mp3"));
+	let settings = parseLightData(await (await fetch("./data/settings.tsv")).text()).sort((a, b) => a.time - b.time);
 	const element = document.querySelector("#output");
+	let intervalId = 0;
 
 	if (midiOutputs.size == 0) {
 		element.innerHTML += `<div>MIDIがなかったよーん</div>`;
@@ -13,44 +14,59 @@ async function sendMidi() {
 
 	{ // 再生処理
 		// 0xB0 = ch1のCC
-		midiOutput.send([0xB0,  1, 0x7f]);
-		midiOutput.send([0xB0,  9, 0x7f]);
+		midiOutput.send([0xB0, 1, 0x7f]);
+		midiOutput.send([0xB0, 9, 0x7f]);
 		midiOutput.send([0xB0, 17, 0x7f]);
 		midiOutput.send([0xB0, 25, 0x7f]);
 
 		arrayBufferSource.start();
 		startTime = Date.now();
 	}
-	setInterval(() => {
+
+	let nowData = new LightData();
+
+	const interval = () => {
 		const nowTime = Date.now() - startTime;
+		if (settings.length === 0) {
+			/// 終了処理
+			clearInterval(intervalId);
+			arrayBufferSource.stop();
+		}
 		if (settings[0].time > nowTime) {
+			if (nowData.time + nowData.fadeTime < nowTime) {
+				return;
+			}
+			const elapse = nowTime - nowData.time;
+			const ratio = elapse / nowData.fadeTime || 0;
+			console.log(elapse, ratio);
+
+			const frontRight = interpolationColor(nowData.beforeData.frontRight, nowData.frontRight, ratio);
+			midiOutput.send([0xB0, 2, frontRight.r]);
+			midiOutput.send([0xB0, 3, frontRight.g]);
+			midiOutput.send([0xB0, 4, frontRight.b]);
+			document.querySelector("#frontRight").setAttribute("color", toHex(frontRight));
+			const frontLeft = interpolationColor(nowData.beforeData.frontLeft, nowData.frontLeft, ratio);
+			midiOutput.send([0xB0, 10, frontLeft.r]);
+			midiOutput.send([0xB0, 11, frontLeft.g]);
+			midiOutput.send([0xB0, 12, frontLeft.b]);
+			document.querySelector("#frontLeft").setAttribute("color", toHex(frontLeft));
+			const rearRight = interpolationColor(nowData.beforeData.rearRight, nowData.rearRight, ratio);
+			midiOutput.send([0xB0, 18, rearRight.r]);
+			midiOutput.send([0xB0, 19, rearRight.g]);
+			midiOutput.send([0xB0, 20, rearRight.b]);
+			document.querySelector("#rearRight").setAttribute("color", toHex(rearRight));
+			const rearLeft = interpolationColor(nowData.beforeData.rearLeft, nowData.rearLeft, ratio);
+			midiOutput.send([0xB0, 26, rearLeft.r]);
+			midiOutput.send([0xB0, 27, rearLeft.g]);
+			midiOutput.send([0xB0, 28, rearLeft.b]);
+			document.querySelector("#rearLeft").setAttribute("color", toHex(rearLeft));
+
 			return;
 		}
-		// 時間に達した
-		element.innerHTML += `<div>${JSON.stringify(settings[0])}</div>`;
-		document.querySelector("#frontRight").setAttribute("color", settings[0].frontRight);
-		const frontRight = parseColor(settings[0].frontRight);
-		midiOutput.send([0xB0,  2, frontRight.r]);
-		midiOutput.send([0xB0,  3, frontRight.g]);
-		midiOutput.send([0xB0,  4, frontRight.b]);
-		document.querySelector("#frontLeft").setAttribute("color", settings[0].frontLeft);
-		const frontLeft = parseColor(settings[0].frontLeft);
-		midiOutput.send([0xB0, 10, frontLeft.r]);
-		midiOutput.send([0xB0, 11, frontLeft.g]);
-		midiOutput.send([0xB0, 12, frontLeft.b]);
-		document.querySelector("#rearRight").setAttribute("color", settings[0].rearRight);
-		const rearRight = parseColor(settings[0].rearRight);
-		midiOutput.send([0xB0, 18, rearRight.r]);
-		midiOutput.send([0xB0, 19, rearRight.g]);
-		midiOutput.send([0xB0, 20, rearRight.b]);
-		document.querySelector("#rearLeft").setAttribute("color", settings[0].rearLeft);
-		const rearLeft = parseColor(settings[0].rearLeft);
-		midiOutput.send([0xB0, 26, rearLeft.r]);
-		midiOutput.send([0xB0, 27, rearLeft.g]);
-		midiOutput.send([0xB0, 28, rearLeft.b]);
 
-		settings.shift();
-	}, 0);
+		nowData = settings.shift();
+	};
+	intervalId = setInterval(interval, 0);
 }
 
 /**
@@ -84,8 +100,12 @@ async function getAudioBufferSource(arrayBuffer) {
 /**
  * 
  * @param {string} data 
+ * @returns {LightData[]}
  */
 function parseLightData(data) {
+	/**
+	 * @type {LightData[]}
+	 */
 	const result = [];
 	for (const row of data.split("\n")) {
 		if (row.startsWith("#")) {
@@ -98,9 +118,22 @@ function parseLightData(data) {
 			frontLeft: cells[2], // 前左ライト色
 			rearRight: cells[3], // 後右ライト色
 			rearLeft: cells[4], // 後左ライト色
+			fadeTime: Number.parseInt(cells[5]), // 切換時間
+			beforeData: result[result.length - 1] || null, // 前のデータ
 		});
 	}
 	return result;
+}
+
+function interpolationColor(a, b, ratio) {
+	const aColor = parseColor(a);
+	const bColor = parseColor(b);
+
+	return {
+		r: aColor.r * (1 - ratio) + bColor.r * ratio,
+		g: aColor.g * (1 - ratio) + bColor.g * ratio,
+		b: aColor.b * (1 - ratio) + bColor.b * ratio,
+	};
 }
 
 async function getMIDI() {
@@ -123,4 +156,18 @@ function parseColor(text) {
 	const b = Math.min(127, Math.floor(Number.parseInt(text.slice(5, 7), 16) / 2));
 
 	return { r, g, b };
+}
+
+function toHex(color) {
+	return `#${Number.parseInt(color.r * 2).toString(16).padStart(2, "0")}${Number.parseInt(color.g * 2).toString(16).padStart(2, "0")}${Number.parseInt(color.b * 2).toString(16).padStart(2, "0")}`;
+}
+
+class LightData {
+	time = 0;
+	frontRight = "#000000";
+	frontLeft = "#000000";
+	rearRight = "#000000";
+	rearLeft = "#000000";
+	fadeTime = 0;
+	beforeData = null;
 }
